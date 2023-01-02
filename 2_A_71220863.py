dc114712d9c23e1ad7e671c8e4059e1095613f21
AkuTerus <66345580+AkuTerus@users.noreply.github.com>
2023-01-02 13:52:27 +0700

    Update 2_A_71220863.py

--- 2_A_71220863.py
+++ 2_A_71220863.py
@@ -6,7 +6,7 @@ diskonKedua = int(totalBelanja-(totalBelanja*0.05))
 diskonKetifa = int(totalBelanja-(totalBelanja*10/100))
 if totalBelanja < 100000:
     print("Tidak ada diskon Maka yang anda bayar adalah Rp ",totalBelanja)
-elif totalBelanja <= 100000:
+elif totalBelanja >= 100000:
     print("Biaya yang harus di bayarkan adalah Rp ",diskonPertama)
 elif totalBelanja >= 500000:
     print("Biaya yang harus di bayarkan adalah Rp ",diskonKedua)
